188637c619a8b8dd659026488fb331ec0e5ab2af
Martin Hjelmare <marhje52@gmail.com>
2020-06-08 02:22:35 +0200

    Add node methods add and remove child (#4)

--- tests/model/test_node.py
+++ tests/model/test_node.py
@@ -3,6 +3,7 @@ import pytest
 from marshmallow.exceptions import ValidationError
 
 from aiomysensors.model.node import Child, Node, NodeSchema
+from aiomysensors.exceptions import AIOMySensorsMissingChildError
 
 NODE_SERIALIZED_FIXTURE = {
     "children": {
@@ -23,6 +24,21 @@ NODE_SERIALIZED_FIXTURE = {
 }
 
 CHILD_FIXTURE = Child(0, 6, description="test child 0", values={0: "20.0"})
+
+
+@pytest.fixture(name="node")
+def node_fixture():
+    """Return a node."""
+    return Node(0, 17, "2.0")
+
+
+@pytest.fixture(name="child")
+def child_fixture(node):
+    """Return a child on a node."""
+    child = node.children[0] = Child(
+        0, 6, description="test child 0", values={0: "20.0"}
+    )
+    return child
 
 
 @pytest.fixture(name="node_schema")
@@ -90,3 +106,36 @@ def test_load_bad_node(node_schema):
     """Test load of bad node."""
     with pytest.raises(ValidationError):
         node_schema.load({"bad": "bad"})
+
+
+def test_add_child(node):
+    """Test add child."""
+    assert not node.children
+
+    child_id = 0
+    child_type = 6
+    description = "test child 0"
+    values = {0: "20.0"}
+
+    node.add_child(child_id, child_type, description, values)
+
+    assert len(node.children) == 1
+    child = node.children[child_id]
+    assert child.child_id == child_id
+    assert child.child_type == child_type
+    assert child.description == description
+    assert child.values == values
+
+
+def test_remove_child(node, child):
+    """Test remove child."""
+    assert child.child_id in node.children
+
+    node.remove_child(child.child_id)
+
+    assert child.child_id not in node.children
+
+    with pytest.raises(AIOMySensorsMissingChildError) as exc:
+        node.remove_child(child.child_id)
+
+        assert exc.child_id == child.child_id
