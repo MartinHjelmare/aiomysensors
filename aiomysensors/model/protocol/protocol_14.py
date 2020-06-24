945e84c8c0705d79427d3bcfd4be38506b3c5ee4
Martin Hjelmare <marhje52@gmail.com>
2020-06-24 17:04:55 +0200

    Add base protocol

---
+++ aiomysensors/model/protocol/protocol_14.py
@@ -0,0 +1,184 @@
+"""Provide the protocol for MySensors version 1.4."""
+from enum import IntEnum
+
+
+class Command(IntEnum):
+    """MySensors command types."""
+
+    presentation = 0
+    set = 1
+    req = 2
+    internal = 3
+    stream = 4
+
+
+class Presentation(IntEnum):
+    """MySensors presentation types."""
+
+    S_DOOR = 0
+    S_MOTION = 1
+    S_SMOKE = 2
+    S_LIGHT = 3
+    S_DIMMER = 4
+    S_COVER = 5
+    S_TEMP = 6
+    S_HUM = 7
+    S_BARO = 8
+    S_WIND = 9
+    S_RAIN = 10
+    S_UV = 11
+    S_WEIGHT = 12
+    S_POWER = 13
+    S_HEATER = 14
+    S_DISTANCE = 15
+    S_LIGHT_LEVEL = 16
+    S_ARDUINO_NODE = 17
+    S_ARDUINO_RELAY = 18
+    S_LOCK = 19
+    S_IR = 20
+    S_WATER = 21
+    S_AIR_QUALITY = 22
+    S_CUSTOM = 23
+    S_DUST = 24
+    S_SCENE_CONTROLLER = 25
+
+
+class SetReq(IntEnum):
+    """MySensors set/req types."""
+
+    V_TEMP = 0  # Temperature
+    V_HUM = 1  # Humidity
+    V_LIGHT = 2  # Light status. 0=off 1=on
+    V_DIMMER = 3  # Dimmer value. 0-100%
+    V_PRESSURE = 4  # Atmospheric Pressure
+    # Weather forecast. One of "stable", "sunny", "cloudy", "unstable",
+    # "thunderstorm" or "unknown"
+    V_FORECAST = 5
+    V_RAIN = 6  # Amount of rain
+    V_RAINRATE = 7  # Rate of rain
+    V_WIND = 8  # Windspeed
+    V_GUST = 9  # Gust
+    V_DIRECTION = 10  # Wind direction
+    V_UV = 11  # UV light level
+    V_WEIGHT = 12  # Weight (for scales etc)
+    V_DISTANCE = 13  # Distance
+    V_IMPEDANCE = 14  # Impedance value
+    # Armed status of a security sensor.  1=armed, 0=bypassed
+    V_ARMED = 15
+    # Tripped status of a security sensor. 1=tripped, 0=untripped
+    V_TRIPPED = 16
+    V_WATT = 17  # Watt value for power meters
+    V_KWH = 18  # Accumulated number of KWH for a power meter
+    V_SCENE_ON = 19  # Turn on a scene
+    V_SCENE_OFF = 20  # Turn off a scene
+    # Mode of heater. One of "Off", "HeatOn", "CoolOn", or "AutoChangeOver"
+    V_HEATER = 21
+    V_HEATER_SW = 22  # Heater switch power. 1=On, 0=Off
+    V_LIGHT_LEVEL = 23  # Light level. 0-100%
+    V_VAR1 = 24  # Custom value
+    V_VAR2 = 25  # Custom value
+    V_VAR3 = 26  # Custom value
+    V_VAR4 = 27  # Custom value
+    V_VAR5 = 28  # Custom value
+    V_UP = 29  # Window covering. Up.
+    V_DOWN = 30  # Window covering. Down.
+    V_STOP = 31  # Window covering. Stop.
+    V_IR_SEND = 32  # Send out an IR-command
+    V_IR_RECEIVE = 33  # This message contains a received IR-command
+    V_FLOW = 34  # Flow of water (in meter)
+    V_VOLUME = 35  # Water volume
+    V_LOCK_STATUS = 36  # Set or get lock status. 1=Locked, 0=Unlocked
+    V_DUST_LEVEL = 37  # Dust level
+    V_VOLTAGE = 38  # Voltage level
+    V_CURRENT = 39  # Current level
+
+
+class Internal(IntEnum):
+    """MySensors internal types."""
+
+    # Use this to report the battery level (in percent 0-100).
+    I_BATTERY_LEVEL = 0
+    # Nodes can request the current time from the Controller using this
+    # message. The time will be reported as the seconds since 1970
+    I_TIME = 1
+    # Nodes report their library version at startup using this message type
+    I_VERSION = 2
+    # Use this to request a unique node id from the controller.
+    I_ID_REQUEST = 3
+    # Id response back to node. Payload contains node id.
+    I_ID_RESPONSE = 4
+    # Start/stop inclusion mode of the Controller (1=start, 0=stop).
+    I_INCLUSION_MODE = 5
+    # Config request from node. Reply with metric (M) or imperial (I) back to node.
+    I_CONFIG = 6
+    # When a node starts up, it broadcast a search request to all neighbor
+    # nodes. They reply with a I_FIND_PARENT_RESPONSE.
+    I_FIND_PARENT = 7
+    # Reply message type to I_FIND_PARENT request.
+    I_FIND_PARENT_RESPONSE = 8
+    # Sent by the gateway to the Controller to trace-log a message
+    I_LOG_MESSAGE = 9
+    # A message that can be used to transfer children
+    # (from EEPROM routing table) of a repeating node.
+    I_CHILDREN = 10
+    # Optional sketch name that can be used to identify node in the
+    # Controller.
+    I_SKETCH_NAME = 11
+    # Optional sketch version that can be reported to keep track of the version
+    # of the node in the Controller.
+    I_SKETCH_VERSION = 12
+    # Used by OTA firmware updates. Request for node to reboot.
+    I_REBOOT = 13
+    # Send by gateway to controller when startup is complete
+    I_GATEWAY_READY = 14
+
+
+class Stream(IntEnum):
+    """MySensors stream types."""
+
+    # Request new FW, payload contains current FW details
+    ST_FIRMWARE_CONFIG_REQUEST = 0
+    # New FW details to initiate OTA FW update
+    ST_FIRMWARE_CONFIG_RESPONSE = 1
+    ST_FIRMWARE_REQUEST = 2  # Request FW block
+    ST_FIRMWARE_RESPONSE = 3  # Response FW block
+    ST_SOUND = 4  # Sound
+    ST_IMAGE = 5  # Image
+
+
+VALID_COMMAND_TYPES = {
+    Command.presentation: list(Presentation),
+    Command.set: list(SetReq),
+    Command.req: list(SetReq),
+    Command.internal: list(Internal),
+    Command.stream: list(Stream),
+}
+
+VALID_MESSAGE_TYPES = {
+    Presentation.S_DOOR: [SetReq.V_TRIPPED, SetReq.V_ARMED],
+    Presentation.S_MOTION: [SetReq.V_TRIPPED, SetReq.V_ARMED],
+    Presentation.S_SMOKE: [SetReq.V_TRIPPED, SetReq.V_ARMED],
+    Presentation.S_LIGHT: [SetReq.V_LIGHT, SetReq.V_WATT],
+    Presentation.S_DIMMER: [SetReq.V_LIGHT, SetReq.V_DIMMER, SetReq.V_WATT],
+    Presentation.S_COVER: [SetReq.V_UP, SetReq.V_DOWN, SetReq.V_STOP, SetReq.V_DIMMER],
+    Presentation.S_TEMP: [SetReq.V_TEMP],
+    Presentation.S_HUM: [SetReq.V_HUM],
+    Presentation.S_BARO: [SetReq.V_PRESSURE, SetReq.V_FORECAST],
+    Presentation.S_WIND: [SetReq.V_WIND, SetReq.V_GUST, SetReq.V_DIRECTION],
+    Presentation.S_RAIN: [SetReq.V_RAIN, SetReq.V_RAINRATE],
+    Presentation.S_UV: [SetReq.V_UV],
+    Presentation.S_WEIGHT: [SetReq.V_WEIGHT, SetReq.V_IMPEDANCE],
+    Presentation.S_POWER: [SetReq.V_WATT, SetReq.V_KWH],
+    Presentation.S_HEATER: [SetReq.V_HEATER, SetReq.V_HEATER_SW, SetReq.V_TEMP],
+    Presentation.S_DISTANCE: [SetReq.V_DISTANCE],
+    Presentation.S_LIGHT_LEVEL: [SetReq.V_LIGHT_LEVEL],
+    Presentation.S_ARDUINO_NODE: [],
+    Presentation.S_ARDUINO_RELAY: [],
+    Presentation.S_LOCK: [SetReq.V_LOCK_STATUS],
+    Presentation.S_IR: [SetReq.V_IR_SEND, SetReq.V_IR_RECEIVE],
+    Presentation.S_WATER: [SetReq.V_FLOW, SetReq.V_VOLUME],
+    Presentation.S_AIR_QUALITY: [SetReq.V_DUST_LEVEL],
+    Presentation.S_CUSTOM: list(SetReq),
+    Presentation.S_DUST: [SetReq.V_DUST_LEVEL],
+    Presentation.S_SCENE_CONTROLLER: [SetReq.V_SCENE_ON, SetReq.V_SCENE_OFF],
+}
