cfa163082d6a9473b0529d2e23e886af6bee7db5
Martin Hjelmare <marhje52@gmail.com>
2020-12-22 23:34:29 +0100

    Test listen invalid message

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -1,6 +1,8 @@
 """Test the gateway."""
 import pytest
 
+from aiomysensors.exceptions import AIOMySensorsInvalidMessageError
+
 # All test coroutines will be treated as marked.
 pytestmark = pytest.mark.asyncio
 
@@ -13,6 +15,17 @@ async def test_listen(gateway, message, message_schema):
         async for msg in gateway.listen():
             assert message_schema.dump(msg) == cmd
             break
+
+
+async def test_listen_invalid_message(gateway, message, message_schema):
+    """Test gateway listen for invalid message."""
+    cmd = message_schema.dump(message)
+    gateway.transport.messages.append("invalid")
+
+    async with gateway.transport:
+        with pytest.raises(AIOMySensorsInvalidMessageError):
+            async for msg in gateway.listen():
+                assert message_schema.dump(msg) == cmd
 
 
 async def test_send(gateway, message, message_schema):
