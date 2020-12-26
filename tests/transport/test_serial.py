ef198b499eaa11d3436fbb6a4711626e5e1650bd
Martin Hjelmare <marhje52@gmail.com>
2020-12-26 17:12:59 +0100

    Complete serial transport tests

--- tests/transport/test_serial.py
+++ tests/transport/test_serial.py
@@ -4,6 +4,11 @@ from unittest.mock import AsyncMock, call, patch
 
 import pytest
 
+from aiomysensors.exceptions import (
+    TransportError,
+    TransportFailedError,
+    TransportReadError,
+)
 from aiomysensors.gateway import Gateway
 from aiomysensors.transport.serial import SerialTransport
 
@@ -31,5 +36,113 @@ async def test_connect_disconnect(serial):
         assert serial.call_args == call(url="/test", baudrate=123456)
 
     _, mock_writer = serial.return_value
+    assert mock_writer.close.call_count == 1
+    assert mock_writer.wait_closed.call_count == 1
+
+
+async def test_connect_failure(serial):
+    """Test serial transport connect failure."""
+    serial.side_effect = OSError("Boom")
+    serial_transport = SerialTransport("/test", 123456)
+    gateway = Gateway(serial_transport)
+
+    with pytest.raises(TransportError):
+        async with gateway.transport:
+            pass
+
+
+async def test_disconnect_failure(serial):
+    """Test serial transport disconnect failure."""
+    _, mock_writer = serial.return_value
+    mock_writer.wait_closed.side_effect = OSError("Boom")
+    serial_transport = SerialTransport("/test", 123456)
+    gateway = Gateway(serial_transport)
+
+    async with gateway.transport:
+        assert serial.call_count == 1
+        assert serial.call_args == call(url="/test", baudrate=123456)
+        # Disconnect error should be caught.
+
+    assert mock_writer.close.call_count == 1
+    assert mock_writer.wait_closed.call_count == 1
+
+
+async def test_read_write(serial):
+    """Test serial transport read and write."""
+    mock_reader, mock_writer = serial.return_value
+    bytes_message = b"0;0;0;0;0;test\n"
+    mock_reader.readuntil.return_value = bytes_message
+    serial_transport = SerialTransport("/test", 123456)
+    gateway = Gateway(serial_transport)
+
+    async with gateway.transport as transport:
+        assert serial.call_count == 1
+        assert serial.call_args == call(url="/test", baudrate=123456)
+
+        read = await transport.read()
+        assert read == "0;0;0;0;0;test\n"
+
+        await transport.write(read)
+        assert mock_writer.write.call_count == 1
+        assert mock_writer.write.call_args == call(bytes_message)
+
+    assert mock_writer.close.call_count == 1
+    assert mock_writer.wait_closed.call_count == 1
+
+
+async def test_read_failure(serial):
+    """Test serial transport read failure."""
+    mock_reader, mock_writer = serial.return_value
+    serial_transport = SerialTransport("/test", 123456)
+    gateway = Gateway(serial_transport)
+
+    async with gateway.transport as transport:
+        assert serial.call_count == 1
+        assert serial.call_args == call(url="/test", baudrate=123456)
+
+        mock_reader.readuntil.side_effect = asyncio.LimitOverrunError(
+            "Boom", consumed=2
+        )
+
+        with pytest.raises(TransportReadError):
+            await transport.read()
+
+        mock_reader.readuntil.side_effect = asyncio.IncompleteReadError(
+            partial=b"partial_test", expected=20
+        )
+
+        with pytest.raises(TransportReadError) as exc_info:
+            await transport.read()
+
+        assert exc_info.value.partial_bytes == b"partial_test"
+
+        mock_reader.readuntil.side_effect = OSError("Boom")
+
+        with pytest.raises(TransportFailedError):
+            await transport.read()
+
+    assert mock_writer.close.call_count == 1
+    assert mock_writer.wait_closed.call_count == 1
+
+
+async def test_write_failure(serial):
+    """Test serial transport write failure."""
+    mock_reader, mock_writer = serial.return_value
+    bytes_message = b"0;0;0;0;0;test\n"
+    mock_reader.readuntil.return_value = bytes_message
+    serial_transport = SerialTransport("/test", 123456)
+    gateway = Gateway(serial_transport)
+
+    async with gateway.transport as transport:
+        assert serial.call_count == 1
+        assert serial.call_args == call(url="/test", baudrate=123456)
+
+        read = await transport.read()
+
+        mock_writer.write.side_effect = OSError("Boom")
+
+        with pytest.raises(TransportFailedError):
+            await transport.write(read)
+
     assert mock_writer.close.call_count == 1
     assert mock_writer.wait_closed.call_count == 1
