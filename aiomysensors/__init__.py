12bfa194c14388ffe2636941aedd78028d5d8c1b
Martin Hjelmare <marhje52@gmail.com>
2020-12-27 20:29:04 +0100

    Document version 1.4 support

--- aiomysensors/__init__.py
+++ aiomysensors/__init__.py
@@ -1,4 +1,9 @@
 """Provide a package for aiomysensors."""
 from pathlib import Path
 
+from .exceptions import AIOMySensorsError
+from .gateway import Config, Gateway  # noqa: F401
+from .transport.serial import SerialTransport  # noqa: F401
+
+__all__ = ["AIOMySensorsError", "Config", "Gateway", "SerialTransport"]
 __version__ = (Path(__file__).parent / "VERSION").read_text().strip()
