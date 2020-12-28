5ef517e621e041b0cfe828fd2348478e55deff6f
Martin Hjelmare <marhje52@gmail.com>
2020-12-28 02:49:15 +0100

    Move fixture

--- tests/model/protocol/conftest.py
+++ tests/model/protocol/conftest.py
@@ -9,3 +9,14 @@ def command_fixture(message_schema, transport, request):
     cmd = message_schema.dump(message)
     transport.messages.append(cmd)
     return cmd
+
+
+@pytest.fixture(name="node_before")
+def node_fixture(gateway, node_schema, request):
+    """Populate a node in the gateway from node data."""
+    node_data = request.param
+    if not node_data:
+        return node_data
+    node = node_schema.load(node_data)
+    gateway.nodes[node.node_id] = node
+    return node
