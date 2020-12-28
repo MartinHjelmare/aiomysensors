23e80ae669eea0fc7c7746e280b1faeddb7bf7bc
Martin Hjelmare <marhje52@gmail.com>
2020-12-28 03:26:31 +0100

    Use missing node child handler

--- tests/model/protocol/test_protocol_20.py
+++ tests/model/protocol/test_protocol_20.py
@@ -3,23 +3,151 @@ from contextlib import ExitStack as default_context
 
 import pytest
 
-from aiomysensors.exceptions import MissingNodeError
+from aiomysensors.exceptions import MissingChildError, MissingNodeError
 from aiomysensors.model.message import Message
 from aiomysensors.model.protocol import PROTOCOL_VERSIONS
-
-from tests.common import NODE_CHILD_SERIALIZED
+from tests.common import (
+    DEFAULT_NODE_CHILD_SERIALIZED,
+    NODE_CHILD_SERIALIZED,
+    NODE_SERIALIZED,
+)
 
 # pylint: disable=too-many-arguments,unused-argument
 
 # All test coroutines will be treated as marked.
 pytestmark = pytest.mark.asyncio
 
-SPECIFIC_PROTOCOL_VERSIONS = list(PROTOCOL_VERSIONS)
-SPECIFIC_PROTOCOL_VERSIONS.remove("1.4")
-SPECIFIC_PROTOCOL_VERSIONS.remove("1.5")
+PROTOCOL_VERSIONS_2x = list(PROTOCOL_VERSIONS)
+PROTOCOL_VERSIONS_2x.remove("1.4")
+PROTOCOL_VERSIONS_2x.remove("1.5")
 
 
-@pytest.mark.parametrize("message_schema", SPECIFIC_PROTOCOL_VERSIONS, indirect=True)
+@pytest.mark.parametrize("message_schema", PROTOCOL_VERSIONS_2x, indirect=True)
+@pytest.mark.parametrize(
+    "command, context, node_before, values_after, writes, reboot",
+    [
+        (
+            Message(0, 0, 1, 0, 0, "25.0"),  # command
+            default_context(),  # context
+            NODE_CHILD_SERIALIZED,  # node_before
+            {0: "25.0"},  # values_after
+            [],  # writes
+            False,  # reboot
+        ),  # Set message
+        (
+            Message(0, 0, 1, 0, 0, "25.0"),  # command
+            default_context(),  # context
+            NODE_CHILD_SERIALIZED,  # node_before
+            {0: "25.0"},  # values_after
+            ["0;255;3;0;13;\n"],  # writes
+            True,  # reboot
+        ),  # Set message, with node reboot true
+        (
+            Message(0, 0, 1, 0, 0, "25.0"),  # command
+            pytest.raises(MissingNodeError),  # context
+            None,  # node_before
+            None,  # values_after
+            ["0;255;3;0;19;\n"],  # writes
+            False,  # reboot
+        ),  # Missing node
+        (
+            Message(0, 0, 1, 0, 0, "25.0"),  # command
+            pytest.raises(MissingChildError),  # context
+            NODE_SERIALIZED,  # node_before
+            None,  # values_after
+            ["0;255;3;0;19;\n"],  # writes
+            False,  # reboot
+        ),  # Missing child
+    ],
+    indirect=["command", "node_before"],
+)
+async def test_set(
+    command,
+    context,
+    node_before,
+    values_after,
+    writes,
+    reboot,
+    gateway,
+    message_schema,
+    node_schema,
+):
+    """Test set command."""
+    if reboot:
+        for node in gateway.nodes.values():
+            node.reboot = True
+
+    with context:
+        async for msg in gateway.listen():
+            assert message_schema.dump(msg) == command
+            break
+
+    for node in gateway.nodes.values():
+        for child in node.children.values():
+            assert child.values == values_after
+
+    assert gateway.transport.writes == writes
+
+
+@pytest.mark.parametrize("message_schema", PROTOCOL_VERSIONS_2x, indirect=True)
+@pytest.mark.parametrize(
+    "command, context, node_before, values_after, writes",
+    [
+        (
+            Message(0, 0, 2, 0, 0),  # command
+            default_context(),  # context
+            NODE_CHILD_SERIALIZED,  # node_before
+            {0: "20.0"},  # values_after
+            ["0;0;1;0;0;20.0\n"],  # writes
+        ),  # Req message
+        (
+            Message(0, 0, 2, 0, 0),  # command
+            default_context(),  # context
+            DEFAULT_NODE_CHILD_SERIALIZED,  # node_before
+            {},  # values_after
+            [],  # writes
+        ),  # Req message, with missing child value
+        (
+            Message(0, 0, 2, 0, 0),  # command
+            pytest.raises(MissingNodeError),  # context
+            None,  # node_before
+            None,  # values_after
+            ["0;255;3;0;19;\n"],  # writes
+        ),  # Missing node
+        (
+            Message(0, 0, 2, 0, 0),  # command
+            pytest.raises(MissingChildError),  # context
+            NODE_SERIALIZED,  # node_before
+            None,  # values_after
+            ["0;255;3;0;19;\n"],  # writes
+        ),  # Missing child
+    ],
+    indirect=["command", "node_before"],
+)
+async def test_req(
+    command,
+    context,
+    node_before,
+    values_after,
+    writes,
+    gateway,
+    message_schema,
+    node_schema,
+):
+    """Test req command."""
+    with context:
+        async for msg in gateway.listen():
+            assert message_schema.dump(msg) == command
+            break
+
+    for node in gateway.nodes.values():
+        for child in node.children.values():
+            assert child.values == values_after
+
+    assert gateway.transport.writes == writes
+
+
+@pytest.mark.parametrize("message_schema", PROTOCOL_VERSIONS_2x, indirect=True)
 @pytest.mark.parametrize(
     "command, writes",
     [
@@ -44,7 +172,7 @@ async def test_gateway_ready(
     assert gateway.transport.writes == writes
 
 
-@pytest.mark.parametrize("message_schema", SPECIFIC_PROTOCOL_VERSIONS, indirect=True)
+@pytest.mark.parametrize("message_schema", PROTOCOL_VERSIONS_2x, indirect=True)
 @pytest.mark.parametrize(
     "command, context, node_before, writes",
     [
