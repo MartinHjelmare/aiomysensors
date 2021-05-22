957ded135d0e25c5cd188eaf3e783180064dae29
Martin Hjelmare <marhje52@gmail.com>
2021-05-22 18:41:21 +0200

    Add preliminary mqtt cli tests

---
+++ tests/cli/test_gateway_mqtt.py
@@ -0,0 +1,56 @@
+"""Test the CLI for the MQTT gateway."""
+from unittest.mock import patch
+
+import pytest
+from click.testing import CliRunner
+
+from aiomysensors.cli import cli
+from aiomysensors.exceptions import (
+    AIOMySensorsError,
+    MissingNodeError,
+    UnsupportedMessageError,
+)
+from aiomysensors.model.message import Message
+
+
+@pytest.fixture(name="gateway_cli", autouse=True)
+def gateway_cli_fixture():
+    """Mock the CLI gateway handler."""
+    with patch("aiomysensors.cli.gateway_mqtt.Gateway", autospec=True) as gateway_class:
+        gateway = gateway_class.return_value
+        yield gateway
+
+
+@pytest.mark.parametrize(
+    "error",
+    [
+        None,
+        MissingNodeError(1),
+        UnsupportedMessageError(Message()),
+        AIOMySensorsError(),
+    ],
+)
+@pytest.mark.parametrize(
+    "args",
+    [
+        [
+            "mqtt-gateway",
+            "-H test.org",
+            "-i mysensors/test-out",
+            "-o mysensors/test-in",
+        ],
+        [
+            "--debug",
+            "mqtt-gateway",
+            "-H test.org",
+            "-i mysensors/test-out",
+            "-o mysensors/test-in",
+        ],
+    ],
+)
+def test_mqtt_gateway(gateway_handler, args, error):
+    """Test the MQTT gateway CLI."""
+    gateway_handler.side_effect = [error, KeyboardInterrupt()]
+    runner = CliRunner()
+    result = runner.invoke(cli, args)
+    assert result.exit_code == 0
