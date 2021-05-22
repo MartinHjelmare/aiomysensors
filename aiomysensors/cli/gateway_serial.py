a556dfa4179aa5b68f1cc3728096905a12628571
Martin Hjelmare <marhje52@gmail.com>
2021-05-22 14:32:17 +0200

    Rework CLI

--- aiomysensors/cli/gateway_serial.py
+++ aiomysensors/cli/gateway_serial.py
@@ -4,7 +4,7 @@ import click
 from aiomysensors.gateway import Gateway
 from aiomysensors.transport.serial import SerialTransport
 
-from .helper import handle_gateway, run_gateway
+from .helper import run_gateway
 
 
 @click.command(options_metavar="<options>")
@@ -19,6 +19,11 @@ from .helper import handle_gateway, run_gateway
 @click.option("-p", "--port", required=True, help="Serial port of the gateway.")
 def serial_gateway(port: str, baud: int) -> None:
     """Start a serial gateway."""
-    transport = SerialTransport(port, baud)
-    gateway = Gateway(transport)
-    run_gateway(handle_gateway, gateway)
+
+    async def gateway_factory() -> Gateway:
+        """Return a gateway."""
+        transport = SerialTransport(port, baud)
+        gateway = Gateway(transport)
+        return gateway
+
+    run_gateway(gateway_factory)
--- aiomysensors/cli/helper.py
+++ aiomysensors/cli/helper.py
@@ -1,35 +1,39 @@
 """Provide CLI helpers."""
 import asyncio
 import logging
-from typing import Callable
+from typing import Awaitable, Callable
 
 from aiomysensors.exceptions import AIOMySensorsError
 from aiomysensors.gateway import Gateway
 
 LOGGER = logging.getLogger("aiomysensors")
 
+GatewayFactory = Callable[[], Awaitable[Gateway]]
 
-def run_gateway(handler: Callable, gateway: Gateway) -> None:
+
+def run_gateway(gateway_factory: GatewayFactory) -> None:
     """Run a gateway."""
     LOGGER.info("Starting gateway")
     try:
-        asyncio.run(start_gateway(handler, gateway))
+        asyncio.run(start_gateway(gateway_factory))
     except KeyboardInterrupt:
         pass
     finally:
         LOGGER.info("Exiting CLI")
 
 
-async def start_gateway(handler: Callable, gateway: Gateway) -> None:
+async def start_gateway(gateway_factory: GatewayFactory) -> None:
     """Start the gateway."""
     try:
-        await handler(gateway)
+        await handle_gateway(gateway_factory)
     except AIOMySensorsError as err:
         LOGGER.error("Error '%s'", err)
 
 
-async def handle_gateway(gateway: Gateway) -> None:
+async def handle_gateway(gateway_factory: GatewayFactory) -> None:
     """Handle the gateway calls."""
+    gateway = await gateway_factory()
+
     async with gateway:  # pragma: no cover
         async for msg in gateway.listen():
             level = logging.DEBUG if msg.message_type == 9 else logging.INFO
