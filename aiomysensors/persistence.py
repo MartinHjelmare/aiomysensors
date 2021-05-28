63719b6d6e21403d1d429238b89a08e93a7ea18d
Martin Hjelmare <marhje52@gmail.com>
2021-05-28 21:00:43 +0200

    Make interface async

--- aiomysensors/persistence.py
+++ aiomysensors/persistence.py
@@ -1,8 +1,9 @@
 """Provide persistence."""
 import json
 from dataclasses import dataclass
-from pathlib import Path
 from typing import Dict
+
+import aiofiles
 
 from .model.node import Node, NodeSchema
 
@@ -14,23 +15,28 @@ class Persistence:
     nodes: Dict[int, Node]
     path: str
 
-    def _load(self, path: str = None) -> None:
-        """Load stored data."""
+    async def load(self, path: str = None) -> None:
+        """Load the stored data."""
         path = path or self.path
-        data: dict = json.loads(Path(path).read_text())
+
+        async with aiofiles.open(path, mode="r") as fil:
+            read = await fil.read()
+
+        data: dict = json.loads(read)
         node_schema = NodeSchema()
         for node_data in data.values():
             node: Node = node_schema.load(node_data)
             self.nodes[node.node_id] = node
 
-    def _save(self) -> None:
+    async def save(self) -> None:
         """Save data."""
         data = {}
         node_schema = NodeSchema()
         for node in self.nodes.values():
             data[node.node_id] = node_schema.dump(node)
 
-        Path(self.path).write_text(json.dumps(data, sort_keys=True, indent=2))
+        async with aiofiles.open(self.path, mode="w") as fil:
+            await fil.write(json.dumps(data, sort_keys=True, indent=2))
 
     async def start(self) -> None:
         """Start the scheduled saving of data."""
