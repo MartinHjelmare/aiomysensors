3124d0958be98f5e2d63492788c972c9e5c26a0f
Martin Hjelmare <marhje52@gmail.com>
2022-07-21 16:59:46 +0200

    chore: cut new package structure

--- setup.py
+++ setup.py
@@ -1,51 +1,9 @@
-"""Set up file for aiomysensors package."""
-from pathlib import Path
+#!/usr/bin/env python
 
-from setuptools import find_packages, setup
+# This is a shim to allow GitHub to detect the package, build is done with poetry
+# Taken from https://github.com/Textualize/rich
 
-PROJECT_DIR = Path(__file__).parent.resolve()
-README_FILE = PROJECT_DIR / "README.md"
-LONG_DESCRIPTION = README_FILE.read_text(encoding="utf-8")
-VERSION = (PROJECT_DIR / "aiomysensors" / "VERSION").read_text().strip()
-GITHUB_URL = "https://github.com/MartinHjelmare/aiomysensors"
-DOWNLOAD_URL = f"{GITHUB_URL}/archive/main.zip"
+import setuptools
 
-REQUIRES = [
-    "aiofiles",
-    "asyncio-mqtt",
-    "awesomeversion",
-    "click",
-    "marshmallow",
-    "pyserial-asyncio",
-]
-
-
-setup(
-    name="aiomysensors",
-    version=VERSION,
-    description="Provide a package for aiomysensors",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    author="Martin Hjelmare",
-    author_email="marhje52@gmail.com",
-    url=GITHUB_URL,
-    download_url=DOWNLOAD_URL,
-    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
-    package_data={"aiomysensors": ["py.typed"]},
-    python_requires=">=3.9",
-    install_requires=REQUIRES,
-    entry_points={"console_scripts": ["aiomysensors = aiomysensors.cli:cli"]},
-    include_package_data=True,
-    license="Apache-2.0",
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Home Automation",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup(name="aiomysensors")
